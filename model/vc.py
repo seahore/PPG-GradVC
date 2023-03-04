b2f9834e3104001e008c7afa2d4bfd61e13b2519
seahore <seahore@whu.edu.cn>
2023-03-04 20:25:44 +0800

    init

---
+++ model/vc.py
@@ -0,0 +1,144 @@
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MIT License for more details.
+
+import torch
+import numpy as np
+
+from model.base import BaseModule
+from model.encoder import MelEncoder
+from model.postnet import PostNet
+from model.diffusion import Diffusion
+from model.utils import sequence_mask, fix_len_compatibility, mse_loss
+
+from conformer_ppg_model.build_ppg_model import load_ppg_model
+
+
+# "average voice" encoder as the module parameterizing the diffusion prior
+class FwdDiffusion(BaseModule):
+    def __init__(self, n_feats, channels, filters, heads, layers, kernel, 
+                 dropout, window_size, dim):
+        super(FwdDiffusion, self).__init__()
+        self.n_feats = n_feats
+        self.channels = channels
+        self.filters = filters
+        self.heads = heads
+        self.layers = layers
+        self.kernel = kernel
+        self.dropout = dropout
+        self.window_size = window_size
+        self.dim = dim
+        self.encoder = MelEncoder(n_feats, channels, filters, heads, layers, 
+                                  kernel, dropout, window_size)
+        self.postnet = PostNet(dim)
+
+    @torch.no_grad()
+    def forward(self, x, mask):
+        x, mask = self.relocate_input([x, mask])
+        z = self.encoder(x, mask)
+        z_output = self.postnet(z, mask)
+        return z_output
+
+    def compute_loss(self, x, y, mask):
+        x, y, mask = self.relocate_input([x, y, mask])
+        z = self.encoder(x, mask)
+        z_output = self.postnet(z, mask)
+        loss = mse_loss(z_output, y, mask, self.n_feats)
+        return loss
+
+
+# the whole voice conversion model consisting of the "average voice" encoder 
+# and the diffusion-based speaker-conditional decoder
+class DiffVC(BaseModule):
+    def __init__(self, n_feats, channels, filters, heads, layers, kernel, 
+                 dropout, window_size, enc_dim, spk_dim, use_ref_t, dec_dim, 
+                 beta_min, beta_max):
+        super(DiffVC, self).__init__()
+        self.n_feats = n_feats
+        self.channels = channels
+        self.filters = filters
+        self.heads = heads
+        self.layers = layers
+        self.kernel = kernel
+        self.dropout = dropout
+        self.window_size = window_size
+        self.enc_dim = enc_dim
+        self.spk_dim = spk_dim
+        self.use_ref_t = use_ref_t
+        self.dec_dim = dec_dim
+        self.beta_min = beta_min
+        self.beta_max = beta_max
+        # self.encoder = PPGModel()
+        self.decoder = Diffusion(n_feats, dec_dim, spk_dim, use_ref_t, 
+                                 beta_min, beta_max)
+
+    def load_encoder(self, conf_path, model_path, device='cuda'):
+        self.encoder = load_ppg_model(conf_path, model_path, device)
+
+    @torch.no_grad()
+    def forward(self, x_wav, x_mel, x_lengths, x_ref, x_ref_lengths, c, n_timesteps, 
+                mode='ml'):
+        """
+        Generates mel-spectrogram from source mel-spectrogram conditioned on
+        target speaker embedding. Returns:
+            1. 'average voice' encoder outputs
+            2. decoder outputs
+        
+        Args:
+            x_wav (torch.Tensor): batch of source wave data.
+            x_mel (torch.Tensor): batch of source mel-spectrograms.
+            x_lengths (torch.Tensor): numbers of frames in source mel-spectrograms.
+            x_ref (torch.Tensor): batch of reference mel-spectrograms.
+            x_ref_lengths (torch.Tensor): numbers of frames in reference mel-spectrograms.
+            c (torch.Tensor): batch of reference speaker embeddings
+            n_timesteps (int): number of steps to use for reverse diffusion in decoder.
+            mode (string, optional): sampling method. Can be one of:
+              'pf' - probability flow sampling (Euler scheme for ODE)
+              'em' - Euler-Maruyama SDE solver
+              'ml' - Maximum Likelihood SDE solver
+        """
+        x_wav, x_mel, x_lengths = self.relocate_input([x_wav, x_mel, x_lengths])
+        x_ref, x_ref_lengths, c = self.relocate_input([x_ref, x_ref_lengths, c])
+        x_mask = sequence_mask(x_lengths).unsqueeze(1).to(x_mel.dtype)
+        x_ref_mask = sequence_mask(x_ref_lengths).unsqueeze(1).to(x_ref.dtype)
+        
+        b = x_mel.shape[0]
+        max_length = int(x_lengths.max())
+        max_length_new = fix_len_compatibility(max_length)
+        x_mel_new = torch.zeros((b, self.n_feats, max_length_new), dtype=x_mel.dtype, 
+                                device=x_mel.device)
+        x_mask_new = sequence_mask(x_lengths, max_length_new).unsqueeze(1).to(x_mel.dtype)
+        
+        for i in range(b):
+            x_mel_new[i, :, :x_lengths[i]] = x_mel[i, :, :x_lengths[i]]
+
+        ppg = [self.encoder(wav.unsqueeze(0), torch.LongTensor([len(wav)]))[0].transpose(0,1).cpu().detach().numpy() for wav in x_wav]
+        ppg = torch.tensor(np.delete(np.array(ppg), slice(max_length_new, None), 2)).cuda()
+
+        z = torch.randn_like(x_mel_new, device=x_mel_new.device)
+
+        y = self.decoder(z, x_mask_new, ppg, x_ref, x_ref_mask, c, 
+                         n_timesteps, mode)
+        return ppg, y[:, :, :max_length]
+
+    def compute_loss(self, x, x_mel, x_lengths, x_ref_mel, c):
+        """
+        Computes diffusion (score matching) loss.
+            
+        Args:
+            x (torch.Tensor): batch of source wave data.
+            x_mel (torch.Tensor): batch of source mel-spectrograms.
+            x_lengths (torch.Tensor): numbers of frames in source mel-spectrograms.
+            x_ref_mel (torch.Tensor): batch of reference mel-spectrograms.
+            c (torch.Tensor): batch of reference speaker embeddings
+        """
+        x, x_mel, x_lengths, x_ref_mel, c = self.relocate_input([x, x_mel,x_lengths, x_ref_mel, c])
+        x_mask = sequence_mask(x_lengths).unsqueeze(1).to(x.dtype)
+        ppg = [self.encoder(wav.unsqueeze(0), torch.LongTensor([len(wav)]))[0].transpose(0,1).cpu().detach().numpy() for wav in x]
+        ppg = torch.tensor(np.delete(np.array(ppg), 128, 2)).cuda()
+        diff_loss = self.decoder.compute_loss(x_mel, x_mask, ppg, x_ref_mel, c)
+        return diff_loss
---
+++ train_dec.py
@@ -0,0 +1,142 @@
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MIT License for more details.
+
+import os
+import numpy as np
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+
+import params
+from data import VCDecDataset, VCDecBatchCollate
+from model.vc import DiffVC
+from model.utils import FastGL
+from utils import save_plot, save_audio
+
+n_mels = params.n_mels
+sampling_rate = params.sampling_rate
+n_fft = params.n_fft
+hop_size = params.hop_size
+
+channels = params.channels
+filters = params.filters
+layers = params.layers
+kernel = params.kernel
+dropout = params.dropout
+heads = params.heads
+window_size = params.window_size
+enc_dim = params.enc_dim
+
+dec_dim = params.dec_dim
+spk_dim = params.spk_dim
+use_ref_t = params.use_ref_t
+beta_min = params.beta_min
+beta_max = params.beta_max
+
+random_seed = params.seed
+test_size = params.test_size
+
+data_dir = 'dataset'
+val_file = 'filelists/valid.txt'
+exc_file = 'filelists/exceptions.txt'
+
+log_dir = 'logs_dec'
+enc_dir = 'logs_enc'
+epochs = 110
+batch_size = 16
+learning_rate = 1e-4
+save_every = 1
+
+
+if __name__ == "__main__":
+
+    torch.manual_seed(random_seed)
+    np.random.seed(random_seed)
+
+    os.makedirs(log_dir, exist_ok=True)
+
+    print('Initializing data loaders...')
+    train_set = VCDecDataset(data_dir, val_file, exc_file)
+    collate_fn = VCDecBatchCollate()
+    train_loader = DataLoader(train_set, batch_size=batch_size, 
+                              collate_fn=collate_fn, num_workers=4, drop_last=True)
+
+    print('Initializing and loading models...')
+    fgl = FastGL(n_mels, sampling_rate, n_fft, hop_size).cuda()
+    model = DiffVC(n_mels, channels, filters, heads, layers, kernel, 
+                   dropout, window_size, enc_dim, spk_dim, use_ref_t, 
+                   dec_dim, beta_min, beta_max).cuda()
+    model.load_encoder(
+        './conformer_ppg_model/en_conformer_ctc_att/config.yaml', 
+        './conformer_ppg_model/en_conformer_ctc_att/24epoch.pth'
+    )
+
+    # print('Encoder:')
+    # print(model.encoder)
+    # print('Number of parameters = %.2fm\n' % (model.encoder.nparams/1e6))
+    # print('Decoder:')
+    # print(model.decoder)
+    # print('Number of parameters = %.2fm\n' % (model.decoder.nparams/1e6))
+
+    print('Initializing optimizers...')
+    optimizer = torch.optim.Adam(params=model.decoder.parameters(), lr=learning_rate)
+
+    print('Start training.')
+    torch.backends.cudnn.benchmark = True
+    iteration = 0
+    for epoch in range(1, epochs + 1):
+        print(f'Epoch: {epoch} [iteration: {iteration}]')
+        model.train()
+        losses = []
+        for batch in tqdm(train_loader, total=len(train_set)//batch_size):
+            wav = batch['wav'].cuda()
+            mel, mel_ref = batch['mel1'].cuda(), batch['mel2'].cuda()
+            c, mel_lengths = batch['c'].cuda(), batch['mel_lengths'].cuda()
+            model.zero_grad()
+            loss = model.compute_loss(wav, mel, mel_lengths, mel_ref, c)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.decoder.parameters(), max_norm=1)
+            optimizer.step()
+            losses.append(loss.item())
+            iteration += 1
+
+        losses = np.asarray(losses)
+        msg = 'Epoch %d: loss = %.4f\n' % (epoch, np.mean(losses))
+        print(msg)
+        with open(f'{log_dir}/train_dec.log', 'a') as f:
+            f.write(msg)
+        losses = []
+
+        if epoch % save_every > 0:
+            continue
+
+        model.eval()
+        print('Inference...\n')
+        with torch.no_grad():
+            mels = train_set.get_valid_dataset()
+            for i, (wav, mel, c) in enumerate(mels):
+                if i >= test_size:
+                    break
+                wav = wav.unsqueeze(0).float().cuda()
+                mel = mel.unsqueeze(0).float().cuda()
+                c = c.unsqueeze(0).float().cuda()
+                mel_lengths = torch.LongTensor([mel.shape[-1]]).cuda()
+                ppg, mel_rec = model(wav, mel, mel_lengths, mel, mel_lengths, c, n_timesteps=100)
+                if epoch == save_every:
+                    save_plot(mel.squeeze().cpu(), f'{log_dir}/original_{i}.png')
+                    save_plot(ppg.squeeze().cpu(), f'{log_dir}/ppg_{i}.png')
+                    audio = fgl(mel)
+                    save_audio(f'{log_dir}/original_{i}.wav', sampling_rate, audio)
+                save_plot(mel_rec.squeeze().cpu(), f'{log_dir}/reconstructed_{i}.png')
+                audio = fgl(mel_rec)
+                save_audio(f'{log_dir}/reconstructed_{i}.wav', sampling_rate, audio)
+
+        print('Saving model...\n')
+        ckpt = model.state_dict()
+        torch.save(ckpt, f=f"{log_dir}/vc_{epoch}.pt")
